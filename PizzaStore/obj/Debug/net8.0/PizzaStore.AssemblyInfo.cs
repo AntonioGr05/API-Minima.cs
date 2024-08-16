314e0b86de49e1b4c0c376ea125f13dd8b37bf57
Antonio Galvan Rojas <agalvanrojas05@gmail.com>
2024-08-16 00:09:12 +0000

    entity framework add endpoints

--- PizzaStore/obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
+++ PizzaStore/obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b66dfe80dabb929b803bfd0c3d24c6d33b16563")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8e74fbc84b696d8c52a528891a104f549c42063")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
